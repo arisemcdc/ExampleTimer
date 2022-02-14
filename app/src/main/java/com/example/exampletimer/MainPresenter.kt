8c05e30fdcdf95b61c39fdac14bc0375bedb58c1
Arisemcdc <arisemcdc1@gmail.com>
2022-02-14 23:45:59 +0300

    Add bind, unbind

--- app/src/main/java/com/example/exampletimer/MainPresenter.kt
+++ app/src/main/java/com/example/exampletimer/MainPresenter.kt
@@ -1,8 +1,33 @@
 package com.example.exampletimer
 
-class MainPresenter (private val model: MainModel) {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+class MainPresenter (private val model: MainModel): CoroutineScope {
+
+    private var view: MainView? = null
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
+
+
+    fun bind(view: MainView) {
+        this.view = view
+    }
+
+    fun unbind() {
+        this.view = null
+    }
 
     fun start() {
-        model.data(13500)
+        launch {
+            model.data(13500)
+            withContext(Dispatchers.Main){
+                view?.showTimer()
+            }
+        }
+
     }
 }
